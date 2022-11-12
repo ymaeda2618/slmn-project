1c226af91ffe2031c2d36ffa97a3235de0bc878f
y_maeda <y.maeda2618@gmail.com>
2022-11-12 20:07:56 +0900

    請求書　encodingの位置訂正

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -1090,11 +1090,10 @@ class DepositController extends Controller
         $pdf = \PDF::view('pdf.pdf_tamplate', [
             'depositList' => $calcDepositList
         ])
+        ->setOption('encoding', 'utf-8')
         ->setOption('footer-left', $company_name)
         ->setOption('footer-center', '[page] ページ')
-        ->setOption('footer-font-size', 8)
-        ->setOption('footer-encoding', 'utf-8')
-        ->setOption('encoding', 'utf-8');
+        ->setOption('footer-font-size', 8);
         return $pdf->inline('invoice_paymentDate' . '_' . $companyId .'.pdf');  //ブラウザ上で開ける
         // return $pdf->download('thisis.pdf'); //こっちにすると直接ダウンロード
     }

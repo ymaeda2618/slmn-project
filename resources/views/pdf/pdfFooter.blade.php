da2d4860ab3bf345971175e5e0e99d6e0bc6606b
y_maeda <y.maeda2618@gmail.com>
2022-11-12 20:38:02 +0900

    請求書 footerHTML化

--- resources/views/pdf/pdfFooter.blade.php
+++ resources/views/pdf/pdfFooter.blade.php
@@ -1,27 +1,26 @@
+<!DOCTYPE html>
+<html>
+
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style type="text/css">
+        * {
+            padding: 0;
+            margin: 0;
+            font-size: 10px;
+            color: #000000;
+            background-color: #ff0000;
+        }
+    </style>
 </head>
 
-<body class="bg-white">
-    <div class="text-center">
-        <span id="pdfkit_page_current"></span>
+<body>
+    @foreach ($pages as $page)
+    <div class="page">
+        {{ $page }}ページ目
     </div>
-    <script type="text/javascript">
-        (function() {
-            var pdfInfo = {};
-            var x = document.location.search.substring(1).split('&');
-            for (var i in x) {
-                var z = x[i].split('=', 2);
-                pdfInfo[z[0]] = unescape(z[1]);
-            }
-            var page = pdfInfo.page || 1; // ページ番号
-            var pageCount = pdfInfo.topage || 1; // 総ページ数
-            // 改ページがある場合のみページ番号を表示します
-            if (pageCount > 1) {
-                document.getElementById('pdfkit_page_current').textContent = page;
-            }
-        })();
-    </script>
+    @endforeach
 </body>
 
 </html>

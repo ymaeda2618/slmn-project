3840c18e2a518b7dbd0a04cff2e4dcdd3e14e5be
y_maeda <y.maeda2618@gmail.com>
2022-11-12 20:05:20 +0900

    請求書　フッターHTML追加

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -1091,9 +1091,10 @@ class DepositController extends Controller
             'depositList' => $calcDepositList
         ])
         ->setOption('footer-left', $company_name)
-        ->setOption('footer-center', '[page] ページ')
-        ->setOption('footer-font-size', 8)
-        ->setOption('footer-font-name', 'Nunito,sans-serif')
+        //->setOption('footer-center', '[page] ページ')
+        //->setOption('footer-font-size', 8)
+        //->setOption('footer-font-name', 'Nunito,sans-serif')
+        ->setOption('footer-html', view('pdf.pdfFooter'))
         ->setOption('encoding', 'utf-8');
         return $pdf->inline('invoice_paymentDate' . '_' . $companyId .'.pdf');  //ブラウザ上で開ける
         // return $pdf->download('thisis.pdf'); //こっちにすると直接ダウンロード

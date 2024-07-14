62562e0ef7a262a862576b6250b3f830600708d8
imai <ryota.i.0320@gmail.com>
2024-07-14 20:11:14 +0900

    売上企業、仕入企業の締日プルダウンリストを31まで表示するように修正

--- resources/views/SaleCompany/create.blade.php
+++ resources/views/SaleCompany/create.blade.php
@@ -63,6 +63,9 @@
                     <option value="26">26</option>
                     <option value="27">27</option>
                     <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
                 </select>
             </div>
             <div class="form-group">

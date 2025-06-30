9b4a34e8a19e5e89bf4cc9362be18291ee7fad5b
y_maeda <y.maeda2618@gmail.com>
2025-06-30 22:09:02 +0900

    メニューが開かない不具合解消

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,9 +115,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Popper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-
     <!-- スクリプトをここでまとめて呼び出す -->
     @stack('scripts')
 

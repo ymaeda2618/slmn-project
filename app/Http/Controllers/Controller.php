a7b24015cd6af001c7c6eb020503f7ddc207d0b3
Ryota Imai <ryotaimai@MyMac.local>
2025-07-09 19:09:54 +0900

    エラーログにuser_idを追加、エラー画面設定処理追加

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $login_user_id;
+
+    public function __construct()
+    {
+        // ログイン済みならユーザーIDを取得
+        $this->middleware(function ($request, $next) {
+            if (\Auth::check()) {
+                $this->login_user_id = \Auth::id(); // 数値形式で取得
+            }
+            return $next($request);
+        });
+    }
+
 }
